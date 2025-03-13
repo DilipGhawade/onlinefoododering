71c6c2a35a377899fb12189598c1ccca53e463ab
dilip94 <ghawadedilip@gmail.com>
2025-03-13 07:44:52 +0530

    add to cart implementation is done

--- app.js
+++ app.js
@@ -10,18 +10,35 @@ function login() {
     alert("Invalid email or password");
   }
 }
-
+let foodList = [
+  {
+    name: "Pizza",
+    image: "images/pizza.png",
+    description: "The pizza is the best option for eat",
+  },
+  {
+    name: "Burger",
+    image: "images/burger.png",
+    description: "The pizza is the best option for eat",
+  },
+  {
+    name: "Pasta",
+    image: "images/pasta.png",
+    description: "The pizza is the best option for eat",
+  },
+  {
+    name: "Salad",
+    image: "images/salad.png",
+    description: "The pizza is the best option for eat",
+  },
+  {
+    name: "Sushi",
+    image: "images/sushi.png",
+    description: "The pizza is the best option for eat",
+  },
+];
 function createFoodList() {
-  var foodList = [
-    { name: "Pizza", image: "images/pizza.png" },
-    { name: "Burger", image: "images/burger.png" },
-    { name: "Pasta", image: "images/pasta.png" },
-    { name: "Salad", image: "images/salad.png" },
-    { name: "Sushi", image: "images/sushi.png" },
-    { name: "Pasta", image: "images/pasta.png" },
-    { name: "Pasta", image: "images/pasta.png" },
-    { name: "Pasta", image: "images/pasta.png" },
-  ];
+  //   if (htmlPage == "index.html") {
 
   var foodContainer = document.createElement("div");
   foodContainer.style.display = "flex";
@@ -32,22 +49,96 @@ function createFoodList() {
     var food = foodList[i];
     var foodItem = document.createElement("div");
     foodItem.style.margin = "10px"; // Add some margin for spacing
+    foodItem.onclick = (function (f) {
+      return function () {
+        addToCart(f, "cart.html");
+      };
+    })(food);
 
-    foodItem.innerHTML = `<img src="${food.image}" alt="${food.name}">`;
-    foodItem.innerHTML += `<p>${food.name}</p>`;
+    foodItem.innerHTML = `<img src="${food.image}" alt="${food.name}" style="width: 50px; height: 50px;">`;
+    foodItem.innerHTML += `<h2>${food.name}</h2>`;
+    foodItem.innerHTML += `<p>${food.description}</p>`;
     foodContainer.appendChild(foodItem);
   }
 
   document.body.appendChild(foodContainer);
+  //   }
 }
-
 var cart = [];
 
-function addToCart(food) {
+function addToCart(food, htmlPage) {
+  //   if (htmlPage == "cart.html") {
+  console.log(`add to card clicked and the food is ${food.name}`);
+
+  //   if (localStorage.getItem("cart")) {
+  //     cart = JSON.parse(localStorage.getItem("cart"));
+  //   }
   cart.push(food);
-  var cartItem = document.createElement("div");
-  cartItem.innerHTML = `<img src="${food.image}" alt="${food.name}">`;
-  cartItem.innerHTML += `<p>${food.name}</p>`;
-  document.body.appendChild(cartItem);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  console.log(cart);
+  // window.location.href = htmlPage;
+  //   }
 }
-createFoodList();
+
+function showCartList() {
+  if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+  }
+  console.log("show cart list is called and the cart list is ", cart);
+
+  var cartContainer = document.createElement("div");
+  cartContainer.style.display = "flex";
+  cartContainer.style.flexWrap = "wrap"; // Allow wrapping to next row
+  cartContainer.style.justifyContent = "center"; // Center items
+  //   if (cart.length == 0) {
+  //     var cartItem = document.createElement("div");
+  //     cartItem.innerHTML = `<h2>Cart is empty</h2>`;
+  //     cartContainer.appendChild(cartItem);
+  //   } else {
+  for (var i = 0; i < cart.length; i++) {
+    var food = cart[i];
+    var cartItem = document.createElement("div");
+    cartItem.style.margin = "10px"; // Add some margin for spacing
+    cartItem.innerHTML = `<img src="${food.image}" alt="${food.name}" style="width: 50px; height: 50px;">`;
+    cartItem.innerHTML += `<p>${food.name}</p>`;
+    cartContainer.appendChild(cartItem);
+  }
+
+  document.body.appendChild(cartContainer);
+  //   }
+}
+
+function serchItem() {
+  const searchInput = document.getElementById("search");
+  searchInput.addEventListener("input", serchItem);
+  const searchValue = searchInput.value.toLowerCase();
+  console.log("search value ", searchValue);
+
+  const filteredItems = foodList.filter((element) =>
+    element.name.toLowerCase().includes(searchValue)
+  );
+
+  // Clear the food list container
+  const foodContainer = document.getElementById("food");
+  foodContainer.innerHTML = "";
+
+  filteredItems.forEach((element) => {
+    const foodItem = document.createElement("div");
+    foodItem.innerHTML = `<img src="${element.image}" alt="${element.name}" style="width: 50px; height: 50px;">`;
+    foodItem.innerHTML += `<h2>${element.name}</h2>`;
+    foodItem.innerHTML += `<p>${element.description}</p>`;
+    foodContainer.appendChild(foodItem);
+  });
+}
+
+// function addToCart(food) {
+//   console.log(`add to card clicked and the food is ${food.name}`);
+
+//   cart.push(food);
+//   console.log(cart);
+//   var cartItem = document.createElement("div");
+//   cartItem.innerHTML = `<img src="${food.image}" alt="${food.name}" style="width: 50px; height: 50px;">`;
+//   cartItem.innerHTML += `<p>${food.name}</p>`;
+//   document.body.appendChild(cartItem);
+// }
+// createFoodList("index.html");
